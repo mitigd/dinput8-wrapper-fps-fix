24d469fc3ddef174e771aded914538baccaebea8
mitigd <a19t89b@gmail.com>
2019-10-19 14:19:05 -0500

    Update dinput8.cpp

--- dinput8.cpp
+++ dinput8.cpp
@@ -2,7 +2,9 @@
 #include "Minhook.h"
 #include "dinput8.h"
 
-HRESULT WINAPI dEnumDevices(
+#pragma comment (lib, "dxguid.lib")
+
+HRESULT WINAPI dEnumDevicesA(
 	LPDIRECTINPUT8A	This,
 	DWORD dwDevType,
 	LPDIENUMDEVICESCALLBACKA lpCallback,
@@ -17,7 +19,27 @@ HRESULT WINAPI dEnumDevices(
 	}
 	else
 	{
-		return pEnumDevicesA(This, dwDevType, lpCallback, pvRef, dwFlags);;
+		return pEnumDevicesA(This, dwDevType, lpCallback, pvRef, dwFlags);
+	}
+
+}
+
+HRESULT WINAPI dEnumDevicesW(
+	LPDIRECTINPUT8W	This,
+	DWORD dwDevType,
+	LPDIENUMDEVICESCALLBACKW lpCallback,
+	LPVOID pvRef,
+	DWORD dwFlags
+)
+{
+
+	if (dwDevType == 1)
+	{
+		return NULL;
+	}
+	else
+	{
+		return pEnumDevicesW(This, dwDevType, lpCallback, pvRef, dwFlags);
 	}
 
 }
@@ -32,16 +54,30 @@ HRESULT WINAPI DirectInput8Create(
 
 	HRESULT ret = oDirectInput8Create(hinst, dwVersion, riidltf, ppvOut, punkOuter);
 
-	LPDIRECTINPUT8A pDIC = static_cast<LPDIRECTINPUT8A>(*ppvOut);
+	LPDIRECTINPUT8A pDICa = static_cast<LPDIRECTINPUT8A>(*ppvOut);
+	LPDIRECTINPUT8W pDICu = static_cast<LPDIRECTINPUT8W>(*ppvOut);
 
-	if (pDIC->lpVtbl->EnumDevices)
+	if (IsEqualIID(riidltf, IID_IDirectInput8A))
 	{
-		oEnumDevicesA = pDIC->lpVtbl->EnumDevices;
+		if (pDICa->lpVtbl->EnumDevices)
+		{
+			oEnumDevicesA = pDICa->lpVtbl->EnumDevices;
 
-		MH_CreateHook((DWORD_PTR*)oEnumDevicesA, dEnumDevices, reinterpret_cast<void**>(&pEnumDevicesA));
-		MH_EnableHook((DWORD_PTR*)oEnumDevicesA);
+			MH_CreateHook((DWORD_PTR*)oEnumDevicesA, dEnumDevicesA, reinterpret_cast<void**>(&pEnumDevicesA));
+			MH_EnableHook((DWORD_PTR*)oEnumDevicesA);
+		}
 	}
+	
+	if (IsEqualIID(riidltf, IID_IDirectInput8W))
+	{
+		if (pDICu->lpVtbl->EnumDevices)
+		{
+			oEnumDevicesW = pDICu->lpVtbl->EnumDevices;
 
+			MH_CreateHook((DWORD_PTR*)oEnumDevicesW, dEnumDevicesW, reinterpret_cast<void**>(&pEnumDevicesW));
+			MH_EnableHook((DWORD_PTR*)oEnumDevicesW);
+		}
+	}
 
 	return ret;
 
@@ -57,7 +93,7 @@ bool WINAPI DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpReserved) {
 	
 		if (MH_Initialize() != MH_OK)
 		{
-			MessageBox(NULL, ("Error"), ("Dinput-FPS-Fix"), MB_OK);
+			MessageBox(NULL, ("Error"), ("Failed to Initialize MinHook"), MB_OK);
 			return 1;
 		}
 
